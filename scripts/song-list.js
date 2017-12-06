424ef70e8b233ad87b2c57f175133d9d593906db
Kelsey Kjeldsen <kelsey.kjeldsen@gmail.com>
2017-12-05 22:25:56 -0500

    checkpoint 12

---
+++ scripts/song-list.js
@@ -0,0 +1,28 @@
+{
+  album.songs.forEach((song, index) => {
+      // create song element with template literal syntax
+      song.element = $(`
+          <tr>
+              <td>
+               <button>
+                 <span class="song-number">${index + 1}</span>
+                 <span class="ion-play"></span>
+                 <span class="ion-pause"></span>
+               </button>
+             </td>
+              <td>${song.title}</td>
+              <td>${song.duration}</td>
+          </tr>
+      `);
+
+      // click handler for song
+      song.element.on('click', event => {
+        player.playPause(song)
+      });
+
+      // append it to the song list
+      $('#song-list').append(song.element);
+
+
+  });
+}
