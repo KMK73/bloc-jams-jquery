4d3fdb87df9b780f37038cb0058c23b09c1fa83f
Kelsey Kjeldsen <kelsey.kjeldsen@gmail.com>
2017-12-05 21:34:20 -0500

    added checkpoint-11 part 2

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,8 @@
 {
   $("#album-title").text(album.title);
   $("img#album-cover-art").attr('src', album.albumArtUrl);
+  //set artist
+  $('.artist').text(album.artist);
+  //set release info
+  $('#release-info').text(album.releaseInfo);
 }
